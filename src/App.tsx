3f34b7cd60a3e4723c864109ed150b5d66a16b72
Tipu Sultan <tipukhan2315@gmail.com>
2023-11-04 13:11:52 +0530

    Build Sort Selector

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import SortSelector from './components/SortSelector';
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 
 }
 
@@ -44,7 +45,7 @@ function App() {
       <GridItem area='main' >
         <HStack spacing={5} paddingLeft={2} marginBottom={5}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
-          <SortSelector />
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
         </HStack>
         <GameGrid gameQuery={gameQuery} selectedGenre={null} selectedPlatform={null} />
       </GridItem>
