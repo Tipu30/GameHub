5c607998165b0077264f601e5a170bcc09649cce
Tipu Sultan <tipukhan2315@gmail.com>
2023-11-05 12:30:50 +0530

    ship genres with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,6 @@
 import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from "../services/image-url";
+import useGenres, { Genre } from "../hooks/useGenres";
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
@@ -19,18 +19,17 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
             <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
             <List >
                 {
-                    data.map(genre =>
+                    data.map(genre => (
                         <ListItem key={genre.id} paddingY='5px'>
                             <HStack>
                                 <Image
                                     boxSize='32px'
                                     borderRadius={8}
                                     objectFit='cover'
-                                    src={getCroppedImageUrl(genre.image_background)} />
+                                    src={getCroppedImageUrl(genre.background_image)} />
                                 <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                             </HStack>
-                        </ListItem>)
-                }
+                        </ListItem>))}
             </List >
         </>
 
